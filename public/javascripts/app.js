19ea4253b86f2419b0508fe1ba055b3bc8888b76
Julian Jelfs <julian.jelfs@gmail.com>
2013-05-31 08:21:29 +0000

    starting to add socket.io. It's falling back to xhr for some reason at the moment which is annoying - should be using web sockets I think

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,15 @@
 $(function(){
+  
+  var socket = io.connect('http://express-box-6905.euw1.actionbox.io');
+  
+  $('#ping').click(function(){
+    socket.emit('ping', { msg : 'hello from snake' });  
+  });
+  
+  socket.on('ping', function(data){
+    console.log(data.msg);  
+  });
+  
   $('#start').click(function(){
     $(this).hide();
             
@@ -7,7 +18,7 @@ $(function(){
       y : 25,
       snakeSize : 20,
       interval : 150,
-      speedUp : false
+      speedUp : true
     }).start();
     
     /*
